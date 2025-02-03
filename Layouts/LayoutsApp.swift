ed3ee914b652a1b26ae91049f76b3d83c263cfcd
Zo-hack <enzoarantes2000@gmail.com>
2025-02-02 21:15:55 -0500

    Initial Commit

---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by Enzo Arantes on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
