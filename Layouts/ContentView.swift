3ef797e7adb470a892b9a797749aa592252296b2
Zo-hack <enzoarantes2000@gmail.com>
2025-02-08 18:21:11 -0500

    Ch. 1 SwiftUI Fundamentals Getting Layout Right - Part 3 Divider, Geometry Reader, & safeArea from Prof G's SwiftUI Course

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = "When The Genius Bar Needs Help They Call You!"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack/*(spacing: 0)squishes views vertically*/ {
+            Text("Awesome!")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.white)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(.indigo)
+            
+            Spacer()
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            
+                .minimumScaleFactor(0.5) //if the text doesn't fit, this shrinks it down
+            
+                .multilineTextAlignment(.center)//aligns text a certain way, such as centered and or trailing/leading
+            
+                .foregroundStyle(.red)
+            
+                .frame(height: 150) //puts the text in a prison/box
+            
+                .frame(maxWidth: .infinity) //stretches the prison/box to the max width
+            
+                .padding() //spaces the text out in all directions
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    messageString = "You Are Awesome!"
+                }
+                Spacer()
+                
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .padding()
+            
+            Rectangle()
+                .frame(height: 0)
+                .background(Color.indigo)
         }
-        .padding()
     }
 }
 
